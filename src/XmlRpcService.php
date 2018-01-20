76c4f6c6f2fe5a20fb3891e86ffc597967b313f2
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-01-20 20:35:47 +0100

    Reject call when a fault has been encountered

--- src/XmlRpcService.php
+++ src/XmlRpcService.php
@@ -5,6 +5,7 @@ namespace ApiClients\Tools\Services\XmlRpc;
 use ApiClients\Foundation\Transport\Service\RequestService;
 use ApiClients\Middleware\Xml\XmlStream;
 use Psr\Http\Message\ResponseInterface;
+use function React\Promise\reject;
 use function React\Promise\resolve;
 use RingCentral\Psr7\Request;
 
@@ -42,6 +43,15 @@ class XmlRpcService
             new XmlStream($xml)
         ))->then(function (ResponseInterface $response) {
             $xml = $response->getBody()->getParsedContents();
+
+            if (isset($xml['methodResponse']['fault'])) {
+                return reject(
+                    new XmlRpcError(
+                        $xml['methodResponse']['fault']['value']['struct']['member'][1]['value']['string'],
+                        (int)$xml['methodResponse']['fault']['value']['struct']['member'][0]['value']['int']
+                    )
+                );
+            }
 
             return resolve($xml['methodResponse']['params']['param']);
         });
